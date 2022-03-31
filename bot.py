90eb98a9c303bb3a772e18b42c5f311fef52eaab
AlexS778 <aleksej.serbinov29@gmail.com>
2022-03-31 22:36:58 +0300

    working with buttons

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import telebot
+from telebot import types
 
 # from bot_token import BOT_TOKEN
 from schedule import ScheduleToday
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot("5125219826:AAGM_KfCH-PvDgqyroyv2OF32QFSMyfDr28")
 
-@bot.message_handler(commands=["start", "help"])
+keyboard = types.ReplyKeyboardMarkup()
+b1 = types.InlineKeyboardButton('button', callback_data='foo')
+keyboard.add(b1)
+
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
@@ -14,6 +19,7 @@ def send_welcome(message):
 def handle_text(message):
     if message.text == "Start":
         bot.send_message(message.chat.id, ScheduleToday())
-
+    if message.text == 'meow':
+        bot.send_message(message.chat.id, 'text', reply_markup=keyboard)
 
 bot.infinity_polling()
